b8b7bcf8119e10fe5450d34a862baf689e067567
Byron Dolon <byronpauldolon@hotmail.com>
2020-11-21 22:59:50 +0100

    Added multiple keyword searching and  video description/thumbnail metadata

--- main.py
+++ main.py
@@ -11,15 +11,14 @@ YOUTUBE_API_VERSION = "v3"
 
 
 def youtube_search(
-	q: str,
-	max_results: int = 50,
+	q: str = None,  # query as string
+	max_results: int = 5,
 	order="relevance",
 	token=None,
 	location=None,
 	location_radius=None,
 ):
 	# instantiate discovery object from googleapiclient
-	# print(DEVELOPER_KEY)
 	youtube = build(
 		YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY
 	)
@@ -32,7 +31,7 @@ def youtube_search(
 			type="video",
 			pageToken=token,
 			order=order,
-			part="id,snippet",  # Part signifies the different types of data you want
+			part="id, snippet",  # which type of data to get
 			maxResults=max_results,
 			location=location,
 			locationRadius=location_radius,
@@ -54,6 +53,8 @@ def youtube_search(
 	category = []
 	tags = []
 	videos = []
+	thumbnails = []
+	descriptions = []
 
 	# fill lists with metadata from search items in the raw search: list
 	for search_result in search_response.get("items", []):
@@ -63,9 +64,9 @@ def youtube_search(
 			videoId.append(search_result["id"]["videoId"])
 
 			video_metadata_response = (
-				youtube.videos()
-				.list(part="statistics, snippet", id=search_result["id"]["videoId"])
-				.execute()
+						youtube.videos()
+						.list(part="statistics, snippet", id=search_result["id"]["videoId"])
+						.execute()
 			)
 
 			video_metadata_items = {"channelId": ("snippet", channelId),
@@ -78,7 +79,9 @@ def youtube_search(
 									"favoriteCount": ("statistics", favoriteCount),
 									"category": ("snippet", category),
 									"tags": ("snippet", tags),
-									"videos": ("snippet", videos)}
+									"videos": ("snippet", videos),
+									"thumbnails": ("snippet", thumbnails),
+									"description": ("snippet", descriptions)}
 
 			# adding all metadata results to metadata lists if they are there
 			for item in video_metadata_items:
@@ -102,6 +105,8 @@ def youtube_search(
 		"dislikeCount": dislikeCount,
 		"commentCount": commentCount,
 		"favoriteCount": favoriteCount,
+		"thumbnails": thumbnails,
+		"descriptions": descriptions
 	}
 
 	next_page_token = search_response.get("nextPageToken")
@@ -111,36 +116,58 @@ def youtube_search(
 
 if __name__ == "__main__":
 	# setting up dataframe of results
-	if not path.exists(f'cat_test_results.csv'):
-		final_results = pd.DataFrame(columns=['tags','channelId','channelTitle' \
-				,'Title','videoId','viewCount','likeCount','dislikeCount' \
-				,'commentCount','favoriteCount'])  # initialize empty df for first time crawl
+	if not path.exists(f'cat_test_results_trial.csv'):
+		final_results = pd.DataFrame()
 	else:
-		final_results = pd.read_csv(f'cat_test_results.csv')  # continue with existing for nth crawl
-		# print(final_results.head())
+		final_results = pd.read_csv(f'cat_test_results_trial.csv')  # continue with existing for nth crawl
 
+	# setting up token for next page, string for query, and list to store all valid results
 	search_next_page = None
-	for i in range(1, 7):  # just change this value for how many pages to run
-		next_page_results, search_next_page = youtube_search("cat videos", token=search_next_page)
-		next_page_df = pd.DataFrame.from_dict(next_page_results)
-		results_to_add = []
-
-		# iterate over table with itertuples (good performance)
-		for result in next_page_df.itertuples():
-			try:
-				if result[6] not in final_results['videoId'].values:
+	search_queries = ["cat videos", "cats"]
+	results_to_add = []
+	
+	# logging variables
+	duplicate_data = 0
+	valid_data = 0
+	
+	# sorry for the nested for loops but ok this goes through the search queries
+	for search_query in search_queries:
+		for i in range(1, 50):  # just change this value for how many pages to run
+			next_page_results, search_next_page = youtube_search(
+				q=search_query,
+				token=search_next_page,
+				max_results=50,
+			)
+			
+			next_page_df = pd.DataFrame.from_dict(next_page_results)
+			next_page_df['search_query'] = search_query  # adding search query column value
+	
+			# iterate over table with itertuples (good performance)
+			for result in next_page_df.itertuples():
+				try:
+					if result[6] not in final_results['videoId'].values:  # result[6] is videoId column value
+						valid_data += 1
+						results_to_add.append(result)
+						print(f"LOGGING: {result[6]} has been added.")
+						continue
+					duplicate_data += 1
+					print(f"WARNING: {result[6]} already exists.")
+				except KeyError:
+					valid_data += 1
 					results_to_add.append(result)
-					# print(f"LOGGING: {result[6]} has been added.")
-					continue
-				# print(f"WARNING: {result[6]} already exists.")
-			except KeyError:
-				results_to_add.append(result)
-
-		final_results = final_results.append(results_to_add, ignore_index=True)
-
-		# print()
-		# print(f"Ran {i} times, page {search_next_page}.")
+					print(f"LOGGING: {result[6]} has been added.")
+			
+			final_results = final_results.append(results_to_add, ignore_index=True)
+	
+			print()
+			print(f"Ran {i} times for search query '{search_query}', page {search_next_page}.")
+			print(f"Valid data found: {valid_data}")
+			print(f"Duplicate data found: {duplicate_data}")
+			print()
 
 	# saving output
-	final_results.to_csv("cat_test_results.csv",index=False)
+	final_results.to_csv("cat_test_results_trial.csv", index=False)
 	# print(final_results.head())
+	
+	# TODO add try except block for the HttpError 403 that comes up when you exceed quota
+	# couldn't test this bc i exceeded my quota already
