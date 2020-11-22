b746e71110214e6931e5a87e26f7f4873506de1e
Byron Dolon <byronpauldolon@hotmail.com>
2020-11-22 13:59:41 +0100

    Tweaked metadata gathering and began EDA

--- README.md
+++ README.md
@@ -5,10 +5,9 @@
 We'll come up with a better name later.
 
 ## Things to do
-- add a try except block for the search request result (api call) for the HttpError 403 comes up when you exceed quota
-- try running many pages (get thousands of results)
 - test multiple keywords manually (find the best ones for actually getting data)
-- start EDA (will do it in a notebook)
+- continue EDA (see attached notebook)
+- set up basic frontend (deciding between flask and streamlit)
 
 Packages managed using pipenv. Check the pipfile for list of dependencies.
 If pipfile lock isn't working, use --pre (need it for the dev dependencies).
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import path, getenv
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import pandas as pd
 
 
@@ -123,51 +124,53 @@ if __name__ == "__main__":
 
 	# setting up token for next page, string for query, and list to store all valid results
 	search_next_page = None
-	search_queries = ["cat videos", "cats"]
+	search_queries = ["funny cats", "cat compilation"]
 	results_to_add = []
 	
 	# logging variables
 	duplicate_data = 0
 	valid_data = 0
 	
-	# sorry for the nested for loops but ok this goes through the search queries
-	for search_query in search_queries:
-		for i in range(1, 50):  # just change this value for how many pages to run
-			next_page_results, search_next_page = youtube_search(
-				q=search_query,
-				token=search_next_page,
-				max_results=50,
-			)
-			
-			next_page_df = pd.DataFrame.from_dict(next_page_results)
-			next_page_df['search_query'] = search_query  # adding search query column value
-	
-			# iterate over table with itertuples (good performance)
-			for result in next_page_df.itertuples():
-				try:
-					if result[6] not in final_results['videoId'].values:  # result[6] is videoId column value
+	# sorry for the nested for loops but ok this goes through the search queries and adjusts for potential quota limit
+	try:
+		for search_query in search_queries:
+			for i in range(1, 20):  # just change this value for how many pages to run
+				next_page_results, search_next_page = youtube_search(
+					q=search_query,
+					token=search_next_page,
+					max_results=50,
+				)
+				
+				next_page_df = pd.DataFrame.from_dict(next_page_results)
+				next_page_df['search_query'] = search_query  # adding search query column value
+		
+				# iterate over table with itertuples (good performance)
+				for result in next_page_df.itertuples():
+					try:
+						if result[6] not in final_results['videoId'].values:  # result[6] is videoId column value
+							valid_data += 1
+							results_to_add.append(result)
+							print(f"LOGGING: {result[6]} has been added.")
+							continue
+						duplicate_data += 1
+						print(f"WARNING: {result[6]} already exists.")
+					except KeyError:
 						valid_data += 1
 						results_to_add.append(result)
 						print(f"LOGGING: {result[6]} has been added.")
-						continue
-					duplicate_data += 1
-					print(f"WARNING: {result[6]} already exists.")
-				except KeyError:
-					valid_data += 1
-					results_to_add.append(result)
-					print(f"LOGGING: {result[6]} has been added.")
-			
-			final_results = final_results.append(results_to_add, ignore_index=True)
+				
+				final_results = final_results.append(results_to_add, ignore_index=True)
+		
+				print()
+				print(f"Ran {i} times for search query '{search_query}', page {search_next_page}.")
+				print(f"Valid data found: {valid_data}")
+				print(f"Duplicate data found: {duplicate_data}")
+				print()
 	
-			print()
-			print(f"Ran {i} times for search query '{search_query}', page {search_next_page}.")
-			print(f"Valid data found: {valid_data}")
-			print(f"Duplicate data found: {duplicate_data}")
-			print()
+	# don't exit when http error just save final results
+	except HttpError:
+		print('WARNING: You have exceeded your daily quota.')
 
 	# saving output
 	final_results.to_csv("cat_test_results_trial.csv", index=False)
 	# print(final_results.head())
-	
-	# TODO add try except block for the HttpError 403 that comes up when you exceed quota
-	# couldn't test this bc i exceeded my quota already
