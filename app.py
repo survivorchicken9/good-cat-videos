3b44388cecc3e83e75445cabc24f5735995daabd
Byron Dolon <byronpauldolon@hotmail.com>
2020-11-25 22:44:28 +0100

    Added templates for extra pages

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from views.about import about_blueprint
 from views.cat_testing import cat_testing_blueprint
-
+from views.process import process_blueprint
+from views.resources import resources_blueprint
 
 app = Flask(__name__)
 
 
 app.register_blueprint(cat_testing_blueprint)
+app.register_blueprint(about_blueprint, url_prefix='/about')
+app.register_blueprint(process_blueprint, url_prefix='/process')
+app.register_blueprint(resources_blueprint, url_prefix='/resources')
 
 
 if __name__ == "__main__":
---
+++ views/resources.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+resources_blueprint = Blueprint("resources", __name__)
+
+
+@resources_blueprint.route("/")
+def index():
+	return render_template("resources.html")
